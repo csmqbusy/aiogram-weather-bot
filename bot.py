06e339942da344b2aa8bdc0545dd1451e6abbb64
csmqbusy <cosmiqeyes@yandex.by>
2024-11-24 17:18:55 +0300

    Fix: small fixes

--- bot.py
+++ bot.py
@@ -25,5 +25,6 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
--- database/models.py
+++ database/models.py
@@ -7,18 +7,16 @@ from database.database import Base
 
 
 class UsersORM(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     tg_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     city: Mapped[str] = mapped_column(nullable=True)
     connection_date: Mapped[datetime] = mapped_column(
-        server_default=text("TIMEZONE ('utc', now())"),
-        nullable=False
+        server_default=text("TIMEZONE ('utc', now())"), nullable=False
     )
     reports: Mapped[list["WeatherReportsORM"]] = relationship(
-        back_populates="user",
-        cascade="all, delete-orphan"
+        back_populates="user", cascade="all, delete-orphan"
     )
 
     def __repr__(self):
@@ -26,13 +24,12 @@ class UsersORM(Base):
 
 
 class WeatherReportsORM(Base):
-    __tablename__ = 'weather_reports'
+    __tablename__ = "weather_reports"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    owner: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    owner: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     date: Mapped[datetime] = mapped_column(
-        server_default=text("TIMEZONE ('utc', now())"),
-        nullable=False
+        server_default=text("TIMEZONE ('utc', now())"), nullable=False
     )
     temp: Mapped[float] = mapped_column(nullable=False)
     feels_like: Mapped[float] = mapped_column(nullable=False)
@@ -40,13 +37,16 @@ class WeatherReportsORM(Base):
     pressure_mm: Mapped[float] = mapped_column(nullable=False)
     visibility: Mapped[float] = mapped_column(nullable=False)
     weather_condition: Mapped[str] = mapped_column(
-        nullable=False, server_default="Данные отсутствуют")
+        nullable=False, server_default="Данные отсутствуют"
+    )
     city: Mapped[str] = mapped_column(nullable=False)
     country: Mapped[str] = mapped_column(nullable=True)
     user: Mapped[list["UsersORM"]] = relationship(back_populates="reports")
 
     def __repr__(self):
-        text = (f"{self.city}, "
-                f"{self.date.day}.{self.date.month}.{self.date.year}, "
-                f"{self.temp} °C")
-        return text
+        message = (
+            f"{self.city}, "
+            f"{self.date.day}.{self.date.month}.{self.date.year}, "
+            f"{self.temp} °C"
+        )
+        return message
