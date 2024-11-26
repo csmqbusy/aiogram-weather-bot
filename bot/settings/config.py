d91350a76d9e70bf02b6059e729825045dc05ecc
csmqbusy <cosmiqeyes@yandex.by>
2024-11-26 13:29:27 +0300

    refactor: Comply with mypy requirements in config.py

--- bot/settings/config.py
+++ bot/settings/config.py
@@ -4,23 +4,23 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-    API_KEY: str
-    BOT_TOKEN: str
-    ADMINS_ID: list
+    DB_HOST: str | None = None
+    DB_PORT: int | None = None
+    DB_USER: str | None = None
+    DB_PASS: str | None = None
+    DB_NAME: str | None = None
+    API_KEY: str | None = None
+    BOT_TOKEN: str | None = None
+    ADMINS_ID: list[int] | None = None
 
     @property
-    def DATABASE_URL_psycopg(self):
+    def DATABASE_URL_psycopg(self) -> str:
         config = (f"{self.DB_USER}:{self.DB_PASS}@"
                   f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
         return f"postgresql+psycopg://{config}"
 
     @property
-    def DATABASE_URL_asyncpg(self):
+    def DATABASE_URL_asyncpg(self) -> str:
         config = (f"{self.DB_USER}:{self.DB_PASS}@"
                   f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
         return f"postgresql+asyncpg://{config}"
