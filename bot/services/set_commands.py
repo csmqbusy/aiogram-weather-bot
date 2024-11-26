2d94cdefd19a41ec2f178736c26a5b6c7b853d82
csmqbusy <cosmiqeyes@yandex.by>
2024-11-26 13:34:47 +0300

    refactor: Comply with mypy requirements in set_commands function

--- bot/services/set_commands.py
+++ bot/services/set_commands.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 from aiogram.types import BotCommand
 
 
-async def set_commands(bot: Bot):
+async def set_commands(bot: Bot) -> None:
     main_menu_commands = [
         BotCommand(command="/start",
                    description="Начало работы с ботом"),
