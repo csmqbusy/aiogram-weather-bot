eba5eee3e09efe63a1fd0d22ba965b0d73755542
csmqbusy <cosmiqeyes@yandex.by>
2024-11-27 11:14:16 +0300

    refactor: Comply with mypy requirements in is_admin filter

--- bot/filters/is_admin.py
+++ bot/filters/is_admin.py
@@ -6,4 +6,6 @@ from bot.settings.config import settings
 
 class IsAdmin(BaseFilter):
     async def __call__(self, message: Message) -> bool:
+        if message.from_user is None:
+            return False
         return message.from_user.id in settings.ADMINS_ID
