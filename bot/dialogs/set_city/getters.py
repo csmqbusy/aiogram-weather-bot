01450c6e12894de8fdf3b1d05420fd6154e53dad
csmqbusy <cosmiqeyes@yandex.by>
2024-11-28 17:02:21 +0300

    refactor: Comply with mypy requirements in set_city getter

--- bot/dialogs/set_city/getters.py
+++ bot/dialogs/set_city/getters.py
@@ -1,9 +1,11 @@
+from typing import Any
+
 from aiogram_dialog import DialogManager
 
 
 async def get_user_city_from_state(
         dialog_manager: DialogManager,
-        **kwargs,
+        **kwargs: dict[str, Any],
 ) -> dict[str, str | None]:
     user_city = dialog_manager.dialog_data.get("user_city")
     return {"user_city": user_city}
