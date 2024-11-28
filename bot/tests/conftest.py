0af974efea856a85494fda4b4c5a0c045b3324e5
csmqbusy <cosmiqeyes@yandex.by>
2024-11-28 21:34:36 +0300

    test: Moved prepare_test_database to a separate conftest.py
    - now it will only work for database unit tests

--- bot/tests/conftest.py
+++ bot/tests/conftest.py
@@ -6,10 +6,7 @@ from aiogram_dialog import setup_dialogs
 
 from bot.handlers import admin_router, user_router
 from bot.dialogs import all_dialogs
-from bot.settings.config import settings
 from bot.tests.mocked_aiogram import MockedBot, MockedSession
-from bot.database.database import async_engine
-from bot.database.models import Base
 
 
 @pytest.fixture(scope="session")
@@ -25,12 +22,3 @@ def bot() -> MockedBot:
     bot = MockedBot()
     bot.session = MockedSession()
     return bot
-
-
-@pytest.fixture(scope="session", autouse=True)
-async def prepare_test_database():
-    assert settings.MODE == "TEST"
-
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
---
+++ bot/tests/test_database/unit_tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bot.settings.config import settings
+from bot.database.database import async_engine
+from bot.database.models import Base
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def prepare_test_database():
+    assert settings.MODE == "TEST"
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
