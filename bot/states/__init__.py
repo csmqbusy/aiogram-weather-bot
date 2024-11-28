ab5d3726b52a4673296ad035a806300276a89a74
csmqbusy <cosmiqeyes@yandex.by>
2024-11-28 12:19:34 +0300

    refactor: Move states to a separate package

---
+++ bot/states/__init__.py
@@ -0,0 +1,21 @@
+from .states import (
+    AdminMenuSG,
+    UsersListSG,
+    UserMenuSG,
+    MyCityWeatherSG,
+    OtherCityWeatherSG,
+    RandomCityWeatherSG,
+    RequestsHistorySG,
+    SetCitySG,
+)
+
+__all__ = [
+    "AdminMenuSG",
+    "UsersListSG",
+    "UserMenuSG",
+    "MyCityWeatherSG",
+    "OtherCityWeatherSG",
+    "RandomCityWeatherSG",
+    "RequestsHistorySG",
+    "SetCitySG",
+]
