d18c508e0883e060856c66a066f9d5a65c87554a
csmqbusy <cosmiqeyes@yandex.by>
2024-11-28 16:55:54 +0300

    fix: Fix the error when trying to set the user's city

--- bot/dialogs/set_city/getters.py
+++ bot/dialogs/set_city/getters.py
@@ -3,6 +3,7 @@ from aiogram_dialog import DialogManager
 
 async def get_user_city_from_state(
         dialog_manager: DialogManager,
+        **kwargs,
 ) -> dict[str, str | None]:
     user_city = dialog_manager.dialog_data.get("user_city")
     return {"user_city": user_city}
