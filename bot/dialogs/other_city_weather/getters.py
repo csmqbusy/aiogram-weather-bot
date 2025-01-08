58cedf92e56952ce2d5aad1c44c8777f15883d0a
csmqbusy <cosmiqeyes@yandex.by>
2025-01-08 15:39:12 +0300

    feat: Implement schemes to all getters

--- bot/dialogs/other_city_weather/getters.py
+++ bot/dialogs/other_city_weather/getters.py
@@ -5,6 +5,10 @@ from aiogram_dialog import DialogManager
 
 from bot.api_requests.weather_request import get_weather_data
 from bot.database.orm import db_client
+from bot.utils.weather_reports import (
+    create_weather_report,
+)
+from bot.utils.cache import get_weather_data_from_cache
 from bot.utils.weather_utils import prepare_weather_data
 
 
@@ -12,20 +16,12 @@ async def get_weather(
         dialog_manager: DialogManager,
         event_from_user: User,
         **kwargs: dict[str, Any],
-) -> dict[str, str]:
+) -> dict[str, str | float]:
     city = dialog_manager.dialog_data["city"]
     user_id = event_from_user.id
     weather_full_data = await get_weather_data(city)
     weather_data = prepare_weather_data(weather_full_data)
-    await db_client.create_weather_report(
-        user_id,
-        float(weather_data["temp"]),
-        float(weather_data["feels_like"]),
-        float(weather_data["wind_speed"]),
-        float(weather_data["pressure"]),
-        weather_data["city"],
-        weather_data["country"],
-        float(weather_data["visibility"]),
-        weather_data["weather_condition"],
-    )
-    return weather_data
+
+    weather_report = create_weather_report(user_id, weather_data)
+    await db_client.add_report(weather_report)
+    return weather_data.model_dump()
--- bot/dialogs/random_city_weather/getters.py
+++ bot/dialogs/random_city_weather/getters.py
@@ -6,6 +6,10 @@ from aiogram_dialog import DialogManager
 
 from bot.api_requests.weather_request import get_weather_data
 from bot.database.orm import db_client
+from bot.utils.weather_reports import (
+    create_weather_report,
+)
+from bot.utils.cache import get_weather_data_from_cache
 from bot.utils.weather_utils import prepare_weather_data
 
 
@@ -13,7 +17,7 @@ async def get_random_city_weather(
         dialog_manager: DialogManager,
         event_from_user: User,
         **kwargs: dict[str, Any],
-) -> dict[str, str]:
+) -> dict[str, str | float]:
     weather_full_data = {"error": "blank"}
     while "error" in weather_full_data:
         random_city = rcoc.get_random_city()
@@ -21,15 +25,6 @@ async def get_random_city_weather(
 
     user_id = event_from_user.id
     weather_data = prepare_weather_data(weather_full_data)
-    await db_client.create_weather_report(
-        user_id,
-        float(weather_data["temp"]),
-        float(weather_data["feels_like"]),
-        float(weather_data["wind_speed"]),
-        float(weather_data["pressure"]),
-        weather_data["city"],
-        weather_data["country"],
-        float(weather_data["visibility"]),
-        weather_data["weather_condition"],
-    )
-    return weather_data
+    weather_report = create_weather_report(user_id, weather_data)
+    await db_client.add_report(weather_report)
+    return weather_data.model_dump()
